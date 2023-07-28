83e323f0a7aadc41d388d8a00777805a46356933
Oscar Diaz <ozk_art@outlook.com>
2023-07-28 02:35:40 -0600

    .env firebase

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,13 +3,13 @@ import {getFirestore} from "firebase/firestore";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyABPImGje_E4wXl2jSIYeku9GIvD9P64M4",
-  authDomain: "proyecto-react-coderhous-ee8ff.firebaseapp.com",
-  projectId: "proyecto-react-coderhous-ee8ff",
-  storageBucket: "proyecto-react-coderhous-ee8ff.appspot.com",
-  messagingSenderId: "1075402881549",
-  appId: "1:1075402881549:web:0343302acbbfbd52dc7344",
-  measurementId: "G-130YCY98ED"
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
+  measurementId: process.env.REACT_APP_measurementId
 };
 
 const app = initializeApp(firebaseConfig);
